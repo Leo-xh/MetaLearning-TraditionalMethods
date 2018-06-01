4d4a4757072432fc82dedd1857ad798f6ca23bf4
throne <1134086740@qq.com>
2018-06-01 20:11:24 +0800

    6.1

---
+++ code/eval.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  1 19:39:17 2018
+
+@author: lenovo
+"""
+
---
+++ code/train.py
@@ -0,0 +1,22 @@
+from keras.models import Model
+from keras.utils import np_utils
+import cifar_input
+import keras
+import json
+import os
+
+bs = [16,32,64,128]
+if os.path.exists("model.h5"):
+    model = keras.models.load_model("model.h5")
+    data_train = cifar_input.cifar10()
+    X_train, Y_train = data_train.cifar10All()
+    Y_train = np_utils.to_categorical(Y_train, num_classes=10)
+    data_test = cifar_input.cifar10('test')
+    X_test, Y_test = data_test.cifar10All()
+    Y_test = np_utils.to_categorical(Y_test, num_classes=10)
+    with open("parameter.txt",'r') as file:
+        best_run = json.loads(file.read())
+    while True:
+        model.fit(x=X_train, y=Y_train, batch_size=bs[best_run['batch_size']], epochs=2, callbacks=[keras.callbacks.EarlyStopping(monitor='acc'),
+                  keras.callbacks.ModelCheckpoint("model.h5", period=5),
+                  keras.callbacks.TensorBoard(write_grads=True, write_images=True)])
