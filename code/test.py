ecee40df6b29d381b81c67cc226dcad6fc655ef7
Leo-xh <1134086740@qq.com>
2018-06-02 11:47:12 +0800

    finished

--- code/test.py
+++ code/test.py
@@ -1,29 +1,21 @@
-# In = Input(shape=(32, 32, 3))
-    # X = In
-    # if conditional({{choice(['BN1True', 'BN1False'])}}) == 'BN1True':
-    #     X = BatchNormalization()(In)
-    # X = Conv2D({{choice([32, 64, 128])}},
-    #            ({{choice([3, 4, 5])}}, {{choice([3, 4, 5])}}))(X)
-    # X = {{choice([MaxPooling2D()(X), AveragePooling2D()(X)])}}
-    # X = Activation('relu')(X)
-    # if conditional({{choice(['BN2True', 'BN2False'])}}) == 'BN2True':
-    #     X = BatchNormalization()(X)
-    # X = Conv2D({{choice([32, 64, 128])}},
-    #            ({{choice([3, 4, 5])}}, {{choice([3, 4, 5])}}))(X)
-    # X = {{choice([MaxPooling2D()(X), AveragePooling2D()(X)])}}
-    # X = Activation('relu')(X)
-    # if conditional({{choice(['BN3True', 'BN3False'])}}) == 'BN3True':
-    #     X = BatchNormalization()(X)
-    # X = Conv2D({{choice([32, 64, 128])}},
-    #            ({{choice([3, 4, 5])}}, {{choice([3, 4, 5])}}))(X)
-    # X = Activation('relu')(X)
-    # X = {{choice([MaxPooling2D()(X), AveragePooling2D()(X)])}}
-    # if conditional({{choice(['BN4True', 'BN4False'])}}) == 'BN4True':
-    #     X = BatchNormalization()(X)
-    # X = Flatten()(X)
-    # X = Dense(10, activation='relu')(X)
-    # X = Activation('softmax')(X)
-    #    if(os.path.exists("model.h5")):
-#        model.load_weights("model.h5")
-#    keras.callbacks.ModelCheckpoint("model.h5", period=5),
-    # model = Model(inputs=In, outputs=X)
+from keras import applications
+from keras.utils import np_utils
+import cifar_input
+import keras
+nas = applications.nasnet
+NASNet = nas.NASNet(input_shape=(32,32,3), penultimate_filters=48, num_blocks=2, 
+                    stem_block_filters=2, classes=10)
+data_train = cifar_input.cifar10()
+X_train, Y_train = data_train.cifar10All()
+Y_train = np_utils.to_categorical(Y_train, num_classes=10)
+data_test = cifar_input.cifar10('test')
+X_test, Y_test = data_test.cifar10All()
+Y_test = np_utils.to_categorical(Y_test, num_classes=10)
+
+NASNet.compile('adam', loss='categorical_crossentropy', metrics=['accuracy'])
+NASNet.fit(x=X_train, y=Y_train, batch_size=32, epochs=30,
+                  validation_data=(X_test, Y_test),
+                  callbacks=[keras.callbacks.EarlyStopping(monitor='acc'),
+                             keras.callbacks.ModelCheckpoint(
+                                 "model.h5", period=5),
+                             keras.callbacks.TensorBoard(write_grads=True, write_images=True)])
